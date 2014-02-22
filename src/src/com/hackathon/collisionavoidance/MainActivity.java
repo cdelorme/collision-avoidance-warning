cd38cdc246ca16fb1f7ddd2faffcb8a2d80bf935
Casey DeLorme <cdelorme@gmail.com>
2014-02-22 00:54:57 -0500

    Added code to launch services for gps and activity service.

--- src/src/com/hackathon/collisionavoidance/MainActivity.java
+++ src/src/com/hackathon/collisionavoidance/MainActivity.java
@@ -1,28 +1,88 @@
 package com.hackathon.collisionavoidance;
 
+import android.util.Log;
+import android.os.Bundle;
+import android.view.Menu;
 import android.app.Activity;
+import android.content.Intent;
 import android.content.Context;
+import android.widget.TextView;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
-import android.os.Bundle;
-import android.util.Log;
-import android.view.Menu;
-import android.widget.TextView;
 
 public class MainActivity extends Activity implements LocationListener {
 
-	private static final String TAG = "MainActivity";
-	private TextView tv;
-	LocationManager lm;
-	Context mCtx;
+	/**
+	 * Gps Service Thread
+	 */
+	protected GpsService gpsService;
 
+	/**
+	 * Wifi Service Thread
+	 */
+	protected WifiService wifiService;
+
+	/**
+	 * Identify State of GpsService
+	 */
+	protected boolean gpsServiceEnabled;
+
+	/**
+	 * Identify State of WifiService
+	 */
+	protected boolean wifiServiceEnabled;
+
+	/**
+	 * Constant activity tag
+	 */
+	public static final String TAG = "MainActivity";
+
+	/**
+	 * Text View (for Activity)
+	 */
+	protected TextView tv;
+
+	/**
+	 * Location Manager (for gps service)
+	 */
+	protected LocationManager lm;
+
+	/**
+	 * ???
+	 */
+	protected Context mCtx;
+
+	/**
+	 * Executed on activity launch
+	 */
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 
-		boolean gps_enabled = false;
-		boolean network_enabled = false;
+		// Set Default States
+		this.gpsServiceEnabled = false;
+		this.wifiServiceEnabled = false;
+
+		// @todo add start button
+		/**
+		 *  Move these to start button click event
+		 */
+		if (this.gpsServiceEnabled != true) {
+			Intent intent = new Intent(this, GpsService.class);
+			startService(intent);
+		}
+
+		/**
+		 *  Move these to start button click event
+		 */
+		if (this.wifiServiceEnabled != true) {
+			Intent intent = new Intent(this, WifiService.class);
+			startService(intent);
+		}
+		// @todo logic to swap text/color to toggle start to stop button
+
+		this.network_enabled = false;
 
 		LocationManager lm = (LocationManager) this
 				.getSystemService(Context.LOCATION_SERVICE);
@@ -30,7 +90,7 @@ public class MainActivity extends Activity implements LocationListener {
 		lm.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, this);
 //
 //		Log.d(TAG, "here");
-//		gps_enabled = lm.isProviderEnabled(LocationManager.GPS_PROVIDER);
+//		gpsServiceEnabled = lm.isProviderEnabled(LocationManager.GPS_PROVIDER);
 //		network_enabled = lm
 //				.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
 //
@@ -38,7 +98,7 @@ public class MainActivity extends Activity implements LocationListener {
 //
 //		Location net_loc = null, gps_loc = null, finalLoc = null;
 //
-//		if (gps_enabled) {
+//		if (gpsServiceEnabled) {
 //			gps_loc = lm.getLastKnownLocation(LocationManager.GPS_PROVIDER);
 //			Log.d(TAG, "here4");
 //		}
@@ -69,9 +129,11 @@ public class MainActivity extends Activity implements LocationListener {
 //			Log.d(TAG, finalLoc.toString());
 //		}
 		setContentView(R.layout.activity_main);
-
 	}
 
+	/**
+	 * Create Options Menu
+	 */
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
@@ -79,6 +141,9 @@ public class MainActivity extends Activity implements LocationListener {
 		return true;
 	}
 
+	/**
+	 * Catch location change event
+	 */
 	@Override
 	public void onLocationChanged(Location location) {
 		// TODO Auto-generated method stub
@@ -89,29 +154,37 @@ public class MainActivity extends Activity implements LocationListener {
 		if ( location.hasSpeed() ) {
 			 s = (location.getSpeed());
 		} else s = 10;
-		
+
 		String disp = "lat:" + lat + " long:" + lng + " time:" + t + " speed:" + s;
 		Log.d(TAG, disp);
 		Log.d(TAG, location.toString());
 
 	}
 
+	/**
+	 * Catch provider disabled
+	 */
 	@Override
 	public void onProviderDisabled(String provider) {
 		// TODO Auto-generated method stub
 
 	}
 
+	/**
+	 * Catch provider enabled
+	 */
 	@Override
 	public void onProviderEnabled(String provider) {
 		// TODO Auto-generated method stub
 
 	}
 
+	/**
+	 * Catch status change
+	 */
 	@Override
 	public void onStatusChanged(String provider, int status, Bundle extras) {
 		// TODO Auto-generated method stub
 
 	}
-
 }
